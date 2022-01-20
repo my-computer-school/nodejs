0f316fa0ee7886cd298384d4e2d6e082c7d78371
my-computer-school <36445022+my-computer-school@users.noreply.github.com>
2022-01-21 01:10:39 +0900

    1

--- test01/client/src/components/CustomerDelete.js
+++ test01/client/src/components/CustomerDelete.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 class CustomerDelete extends React.Component {
 
