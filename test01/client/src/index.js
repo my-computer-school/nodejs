5d8edd411fd4143a9812528c14f54dca4fcec121
my-computer-school <36445022+my-computer-school@users.noreply.github.com>
2022-01-21 01:04:32 +0900

    1

--- test01/client/src/index.js
+++ test01/client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { createTheme } from '@mui/material/styles';
 
-const theme = createMuiTheme({
+const theme = createTheme({
     typography: {
         fontFamily: '"Noto Sans KR", serif',
     }
 })
 
-ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'));
+ReactDOM.render(<createTheme theme={theme}><App /></createTheme>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
